6a3643c53b6c961d62944a68d93a22f3f4e94328
cmiller36 <m.calliemiller@gmail.com>
2017-02-15 18:24:12 -0500

    update user model and user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }
 end
