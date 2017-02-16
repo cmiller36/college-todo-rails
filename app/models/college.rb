175eef88f4209f1ad534d8bb00d4392168048e1c
cmiller36 <m.calliemiller@gmail.com>
2017-02-15 22:14:20 -0500

    create date validation, users can not add past dates

--- app/models/college.rb
+++ app/models/college.rb
@@ -2,6 +2,17 @@ class College < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name
+  validate :college_visit_cannot_be_in_the_past
+
+  def self.pop_colleges
+    College.group(:name).order('count_id DESC').limit(5).count(:id)
+  end
+
+  def college_visit_cannot_be_in_the_past
+    if college_visit.present? && college_visit < Date.today
+      errors.add(:college_visit, "can not be in the past, please choose a future date!")
+    end
+  end    
   
 
 end
