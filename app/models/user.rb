d47c33f077e863da1a2f8518ff65d57f61a18b5e
cmiller36 <m.calliemiller@gmail.com>
2017-02-15 20:33:47 -0500

    update user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook]
 
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }
+
+  has_many :colleges, dependent: :destroy
+
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.username = auth.info.name
+        user.password = Devise.friendly_token[0,20]
+        user.password_confirmation = user.password
+        user.save!
+      end
+    end
+
+
 end
