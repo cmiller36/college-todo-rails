aa246a8580e681f3de9b051f055caada5a4f056a
cmiller36 <m.calliemiller@gmail.com>
2017-02-16 15:43:16 -0500

    migrated from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170216030246) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "colleges", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
@@ -33,8 +36,8 @@ ActiveRecord::Schema.define(version: 20170216030246) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["story_id"], name: "index_comments_on_story_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["story_id"], name: "index_comments_on_story_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "stories", force: :cascade do |t|
@@ -43,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170216030246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_stories_on_user_id"
+    t.index ["user_id"], name: "index_stories_on_user_id", using: :btree
   end
 
   create_table "story_tags", force: :cascade do |t|
@@ -75,10 +78,10 @@ ActiveRecord::Schema.define(version: 20170216030246) do
     t.string   "provider"
     t.string   "uid"
     t.string   "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["provider"], name: "index_users_on_provider"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid"], name: "index_users_on_uid"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["provider"], name: "index_users_on_provider", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["uid"], name: "index_users_on_uid", using: :btree
   end
 
 end
