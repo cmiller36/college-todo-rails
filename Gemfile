1e699d41ed2188e75b1c3358d557d16126df33fe
cmiller36 <m.calliemiller@gmail.com>
2017-02-16 16:26:20 -0500

    added faker gem and user seed data

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "font-awesome-rails"
 
 gem 'kaminari'
 
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+## Users ##
+User.create(
+  username: 'Sparklecat',
+  email: "sparkle@email.com",
+  password: "test123",
+  password_confirmation: "test123",
+)
+
+User.create(
+  username: "dalecooper",
+  email: "thecoop@email.com",
+  password: 'password',
+  password_confirmation: 'password'
+)
+User.create(
+  username: "laurapalmer",
+  email: "lpalmer@email.com",
+  password: 'password',
+  password_confirmation: 'password'
+)
+User.create(
+  username: "loglady",
+  email: "theloglady@email.com",
+  password: 'password',
+  password_confirmation: 'password'
+)
+User.create(
+  username: "GobBluth",
+  email: "gob@email.com",
+  password: 'password',
+  password_confirmation: 'password'
+)
+User.create(
+  username: "Tobias",
+  email: "analrapist@email.com",
+  password: 'password',
+  password_confirmation: 'password'
+)
