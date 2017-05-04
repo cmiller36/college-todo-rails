6b1434d1fad865a3091a3efc0c90c86aabccf7c3
cmiller36 <m.calliemiller@gmail.com>
2017-05-04 12:20:36 -0500

    update story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,10 +2,11 @@ class Story < ApplicationRecord
 
   belongs_to :user
   has_many :story_tags
-  has_many :tags, :through => :story_tags
+  has_many :tags, :through => :story_tags, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates_presence_of :title, :content
+  validates :title, presence: true
+  validates :content, presence: true
   validates :user_id, presence: true
 
   accepts_nested_attributes_for :tags, reject_if: :all_blank
