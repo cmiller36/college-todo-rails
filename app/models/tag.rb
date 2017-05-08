f19eb989cdbedca5196dde08fa231c3a0c1df305
cmiller36 <m.calliemiller@gmail.com>
2017-05-08 17:30:11 -0500

    update validation macro

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   has_many :story_tags
   has_many :stories, :through => :story_tags
 
-  validates_uniqueness_of :name
+  validates :name, uniqueness: { case_sensitive: false }
 
   extend FriendlyId
   friendly_id :name, use: :slugged
