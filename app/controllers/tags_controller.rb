7fdfda6ca808f9777f019a45c9067b26b46f3387
cmiller36 <m.calliemiller@gmail.com>
2017-07-15 16:27:54 -0500

    update tag controller to render json

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,4 +5,19 @@ class TagsController < ApplicationController
     @tag = Tag.friendly.find(params[:id])
   end
 
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      render json: @tag
+    else
+      render json: {errors: @tag.errors.full_messages}
+    end
+  end
+
+  private
+
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
+
 end
