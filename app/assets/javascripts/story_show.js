88a9dec01b2e183b4a6641e3fd8ebe0eb0dc8d8a
cmiller36 <m.calliemiller@gmail.com>
2017-07-15 16:18:58 -0500

    add jquery for story show page

---
+++ app/assets/javascripts/story_show.js
@@ -0,0 +1,34 @@
+var Story = function(attributes) {
+  this.id = attributes.id;
+  this.title = attributes.title;
+  this.content = attributes.content;
+  this.slug = attributes.slug;
+  this.created_at = attributes.created_at;
+  this.comments = attributes.comments;
+  this.user = attributes.user;
+};
+
+Story.prototype.renderStory = function() {
+  var text = "";
+  text = '<h2 class="blog-post-title">' + this.title + '</h2>';
+  text += '<p class="blog-post-meta">' + 'by' + ' ' + this.user.username + ' ' +'-' + ' ' + this.created_at + ' ' + '</p>';
+  text += '<p class="blog-post-content">' + this.content + '</p>';
+  return text;
+};
+
+
+var getStory = function() {
+    $.ajax({
+      url: this.href,
+      method: "GET",
+      dataType: "json",
+    }).success(function(json){
+      var story = new Story(json)
+      var renderStory = story.renderStory();
+      $("#showstory").append(renderStory);
+    })
+}
+  
+$(document).ready(function() {
+  getStory();
+})
