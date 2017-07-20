a01c16ceddb30b550fc40eac00ec07852056808b
cmiller36 <m.calliemiller@gmail.com>
2017-07-20 17:12:19 -0500

    update timestamp for when comment is created

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :content, :created_at, :user
   has_one :story
+
+  def created_at
+   object.created_at.strftime("%a %m-%d-%Y")
+  end
 end
+
