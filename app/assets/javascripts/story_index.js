dfa689c7fdde216b163c928df704bf8a0e29099c
cmiller36 <m.calliemiller@gmail.com>
2017-07-21 12:49:33 -0500

    add story link to title

--- app/assets/javascripts/story_index.js
+++ app/assets/javascripts/story_index.js
@@ -6,7 +6,7 @@ $(function(){
       $stories.html("")
       
       json.forEach(function(story){
-        text = '<h2 class="blog-post-title">' + story.title + '</h2>';
+        text = '<h2 class="blog-post-title"><a class="js-story-show" href="/stories/' + story.slug + '">' + story.title + '</a></h2>';
         text += '<p class="blog-post-meta">' + 'by' + ' ' + story.user.username + ' ' +'-' + ' ' + story.created_at +
                 ' ' + '<span class="badge">' + story.comments_count + ' ' +'comments' + '</span></p>';
         text += '<p>' + story.content.substring(0, 250) + '...';
