610deca9f8499ccc4bbaeb5dda52e8d5ffff023d
cmiller36 <m.calliemiller@gmail.com>
2017-07-23 17:04:54 -0500

    tried vary response header then deleted it, worked but still needed a hard refresh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :exception
 
   private
@@ -8,5 +7,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
   end
+
 
 end
